f530149379bcdbaf3298d6eb26d05c6ffafb9862
hancco05 <s.carmonawright@gmail.com>
2025-09-16 23:43:27 -0300

    commit3

--- public/index.php
+++ public/index.php
@@ -1,10 +1,18 @@
-<form method="POST" action="../src/login.php">
-    <label>Usuario:</label>
-    <input type="text" name="usuario" required>
-    <br>
-    <label>Contraseña:</label>
-    <input type="password" name="contraseña" required>
-    <br>
-    <button type="submit">Iniciar sesión</button>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+<form method="post" action="../src/login.php">
+    <input type="text" name="usuario" placeholder="Usuario" required>
+    <input type="password" name="contraseña" placeholder="Contraseña" required>
+    <button type="submit">Ingresar</button>
+    <p>¿No tienes cuenta? <a href="registro.php">Regístrate aquí</a></p>
 </form>
-<p>¿No tienes cuenta? <a href="registro.php">Regístrate aquí</a></p>
+
+</body>
+</html>
